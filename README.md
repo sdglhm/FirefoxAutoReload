2377d6f0fc32c724234d316924c2166746f1099d
U-CR-PC-04\Lahiru Himesh <lahiru.lahirumadusanka@gmail.com>
2016-06-25 06:14:29 +0530

    Finish project

---
+++ README.md
@@ -0,0 +1,2 @@
+#Firefox AutoRefresh
+This addon will work on your browser to refresh and browse URL for Fiverr
---
+++ index.js
@@ -0,0 +1,56 @@
+var contextMenu = require("sdk/context-menu");
+var tabs = require("sdk/tabs");
+var utils = require("sdk/tabs/utils").id;
+var self = require("sdk/self");
+var {setInterval, clearInterval} = require('sdk/timers')
+
+var reloadIsOn = false;
+var current;
+
+var menuItem = contextMenu.Menu({
+	label: "Auto-Reload",
+	context: contextMenu.URLContext("*.fiverr.com/*"),
+	contentScript: "self.on('click', function(node, data) {self.postMessage(data, node.id);});",
+	image: self.data.url("refresh-icon.png"),
+	items: [
+	contextMenu.Item({
+		label: "Reload every 3 minutes", 
+		data: "reload3m",
+
+	}),
+	contextMenu.Item({ 
+		label: "Reload every 5 minutes",
+		data: "reload5m" 
+	}),
+	contextMenu.Separator({
+
+	}),
+	contextMenu.Item({ 
+		label: "Stop Reload", 
+		data: "reloadstop",
+		image: self.data.url("stop.png")
+	})
+	],
+	
+	onMessage: function(data, tabs) {
+		console.log(data);
+		var tabs = require("sdk/tabs");
+		var tabId = tabs.activeTab;
+		
+		if (data == "reload3m") {
+			current = setInterval(function(data){
+				tabId.reload();
+				console.log("Reloaded for 3 minutes");
+			},180000);
+		}else if (data == "reload5m"){
+			current = setInterval(function(data){
+				tabId.reload();
+			},300000);
+			console.log("Reloaded for 5 minutes");
+		}else if (data == "reloadstop"){
+			console.log("Reloaded ended");
+			clearInterval(current);
+		}
+	}
+});
+
